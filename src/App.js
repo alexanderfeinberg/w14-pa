d64d78b4bd460e1efd926aa80a716024b93720de
alexander1fein <feinberg.alexander@gmail.com>
2022-10-07 18:50:00 -0400

    Phase 3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import FruitsIndex from "./components/FruitsIndex";
 import FruitForm from "./components/FruitForm";
 import { Route } from "react-router-dom";
 
+import FavoriteFruit from "./components/FavoriteFruit";
+
 function App() {
   return (
     <div>
       <FruitsIndex fruits={fruits} />
       <FruitForm fruits={fruits} />
+      <FavoriteFruit fruits={fruits} />
     </div>
   );
 }
--- src/components/FavoriteFruit.js
+++ src/components/FavoriteFruit.js
@@ -1,5 +1,18 @@
-const FavoriteFruit = () => {
-  return null;
-}
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { FavFruitContext } from "../context/FavFruitContext";
+
+const FavoriteFruit = ({ fruits }) => {
+  const { favFruitId, setFavFruitId } = useContext(FavFruitContext);
+  let [name] = fruits.filter((fruit) => fruit.id === favFruitId);
+  let { name: fruitName } = name;
+  console.log(name);
+  return (
+    <div className="fav-fruit">
+      <h2>Favorite Fruit</h2>
+      <NavLink to={`/fruits/${favFruitId}`}>{fruitName}</NavLink>
+    </div>
+  );
+};
 
 export default FavoriteFruit;
