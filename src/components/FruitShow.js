6034a251eb8147b28c6413bf219ccc75686c5452
alexander1fein <feinberg.alexander@gmail.com>
2022-10-08 11:11:02 -0400

    Phase7&8

--- src/components/FruitShow.js
+++ src/components/FruitShow.js
@@ -1,5 +1,27 @@
-function FruitShow() {
+import { useParams } from "react-router-dom";
+
+const findFruit = (id, fruits) => {
+  id = Number(id);
+  for (let fruit of fruits) {
+    if (fruit.id === id) {
+      return fruit;
+    }
+  }
   return null;
+};
+
+function FruitShow({ fruits }) {
+  let { fruitId } = useParams();
+  let fruit = findFruit(fruitId, fruits);
+
+  return (
+    <div class="fruit-show">
+      <h2>{fruit.name}</h2>
+      <p>{fruit.color}</p>
+      <p>{fruit.sweetness}</p>
+      <p>{fruit.seeds}</p>
+    </div>
+  );
 }
 
 export default FruitShow;
