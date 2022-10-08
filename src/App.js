74c24f84de300f9ce2d459452a0fe2abf78773ab
alexander1fein <feinberg.alexander@gmail.com>
2022-10-08 09:39:17 -0400

    Phase5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import fruits from "./mockData/fruits.json";
 import FruitsIndex from "./components/FruitsIndex";
 import FruitForm from "./components/FruitForm";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SetFavoriteFruit from "./components/SetFavoriteFruit";
 import FavoriteFruit from "./components/FavoriteFruit";
+import FruitShow from "./components/FruitShow";
 
 function App() {
   return (
     <div>
-      <FruitsIndex fruits={fruits} />
-      <FruitForm fruits={fruits} />
-      <FavoriteFruit fruits={fruits} />
-      <SetFavoriteFruit fruits={fruits} />
+      <Switch>
+        <Route exact path="/" render={() => <FruitsIndex fruits={fruits} />} />
+        <Route
+          path="/fruits/new"
+          render={() => <FruitForm fruits={fruits} />}
+        />
+        <Route
+          path="/fav-fruit"
+          render={() => <FavoriteFruit fruits={fruits} />}
+        />
+
+        <Route
+          path="/set-fav-fruit"
+          render={() => <SetFavoriteFruit fruits={fruits} />}
+        />
+        <Route
+          path={`/fruits/:fruitId`}
+          render={() => <FruitShow fruits={fruits} />}
+        />
+        <Route render={() => <div>Page Not Found</div>} />
+      </Switch>
     </div>
   );
 }
